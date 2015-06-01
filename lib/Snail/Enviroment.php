06d8d139421ebc7cb7d3977f7dff045eb6cec662
fastin <jarvis-1@mail.ru>
2015-06-01 13:59:20 +0500

    Update files

--- lib/Snail/Enviroment.php
+++ lib/Snail/Enviroment.php
@@ -4,7 +4,7 @@
  *
  *	@author		Author: fastin. (https://github.com/fastin)
  *	@git		https://github.com/fastin/snail
- *	@version	0.1
+ *	@version	0.2
  *	@license	MIT License. (https://github.com/fastin/Snail/blob/master/LICENSE)
  */
 class Snail_Enviroment
